663050f34a106368e0f42c31dfe8e1d1f494254c
Jerson Huaman <jerson.huaman.11@outlook.com>
2022-02-23 21:11:16 -0500

    Editando Count , Y Haciendo un efecto cuando llega al stock maximo del producto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
 import ItemListContainer from './components/Item/ItemListContainer';
 import ItemDetailContainer from './components/Item/ItemDetailContainer';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from "react-use-cart";
 import Error404 from './components/pages/Error404';
-import Slider from './components/Slider';
 
 function App() {
     return (
         <CartProvider>
-            <Router>
+            <BrowserRouter>
                 <Navbar />
-                <Slider/>
+
                 <Routes>
                     <Route path="/" element={<ItemListContainer />} />
                     <Route path="/category/:id" element={<ItemListContainer />} />
                     <Route path="/item/:id" element={<ItemDetailContainer />} />
                     <Route path="*" element={<Error404/>}/>
                 </Routes>
-            </Router>
+            </BrowserRouter>
         </CartProvider>
     );
 }
