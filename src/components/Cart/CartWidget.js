50b3feda39459eec7c7cfc1f9db6512555522175
Jerson Huaman <jerson.huaman.11@outlook.com>
2022-03-01 13:20:56 -0500

    VIsualizando Cart

--- src/components/Cart/CartWidget.js
+++ src/components/Cart/CartWidget.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import '../Css/_CartWidget.css';
@@ -6,9 +7,12 @@ import { Link } from 'react-router-dom'
 
 
 const CartWidget = () => {
+    const {cart, totalCart} = useContext(CartContext)
     return (
-        <Link to='/cart' >
-            <FontAwesomeIcon icon={faShoppingCart} />
+        <Link to='/cart' className={cart.length > 0 ? 'nav__cart nav__cart--show' : 'nav__cart'}>
+            <FontAwesomeIcon icon={faShoppingCart} className="nav__carrito"/>
+            <span className='spanCart'>{cart.reduce((acc, el) => acc + el.quantity, 0)}</span>
+            <p className="cartW-precio">${totalCart().toFixed(2)}</p>
         </Link>
     )
 }
