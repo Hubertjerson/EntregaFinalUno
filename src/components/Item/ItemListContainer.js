a03942d9c5dc5c1c48d607fe04a8b5b0b327a33e
Jerson Huaman <jerson.huaman.11@outlook.com>
2022-03-09 21:46:52 -0500

    Agregando Firebase

--- src/components/Item/ItemListContainer.js
+++ src/components/Item/ItemListContainer.js
@@ -1,28 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import ItemList from './ItemList';
-import { products } from './items';
+//import { products } from './items';
 import { useParams } from 'react-router';
-import Category from '../Navbar/Category';
+import { collection, getDocs } from 'firebase/firestore';
+import { Loader } from '../pages/Loader';
+import db from '../firebase/firebase'
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
+    const [load, setLoad] = useState (true)
     const { id } = useParams();
 
-    useEffect(() => {
-        const traerProductos = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(products);
-            }, 2000);
-        });
-        traerProductos
-            .then((res) => {
-                const categorias = res.filter((i) => i.category === `${id}`);
-                id === undefined ? setItems(res) : setItems(categorias);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, [id]);
+//    useEffect(() => {
+//        const traerProductos = new Promise((resolve, reject) => {
+//            setTimeout(() => {
+//                resolve(products);
+//            }, 2000);
+//        });
+//        traerProductos
+//            .then((res) => {
+//                const categorias = res.filter((i) => i.category === `${id}`);
+//                id === undefined ? setItems(res) : setItems(categorias);
+//            })
+//            .catch((error) => {
+//                console.error(error);
+//            });
+//    }, [id]);
+
+const getData = async () =>{
+    try{
+        const itemColecion = collection(db,"items")
+        const col = await getDocs(itemColecion)
+        const result = col.docs.map((doc) => doc = { id :doc.id, ...doc.data()})
+        setItems(result)
+        setLoad(false)
+
+    } catch(error){
+        console.warn("error",error)
+    }
+}
+
+
+const getDataCategory = async () =>{
+    try{
+        const itemColecion = collection(db,"items")
+        const col = await getDocs(itemColecion)
+        const result = col.docs.map((doc) => doc = { id :doc.id, ...doc.data()})
+        setItems(result)
+        setItems (result.filter(e=>e.category === id))
+        setLoad(false)
+
+    } catch(error){
+        console.warn("error",error)
+    }
+}
+
+
+
+useEffect(()=>{
+    id? getDataCategory():getData()
+},[id])
+
+console.log("items:",items)
 
     return (
         <div className='container my-5 py-5 text-decoration-none'>
@@ -32,8 +71,7 @@ const ItemListContainer = () => {
                 </div>
             </div>
             <div className='row justify-content-center text-decoration-none'>
-                <Category/>
-                <ItemList items={items} />
+                {load? <Loader/> : <ItemList items={items} />}
             </div>
         </div>
     );
---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,17 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrNcTvOFQ-hZD03AgGpa9Wux9xclcjp-o",
+  authDomain: "proyecto-coderhouse-79233.firebaseapp.com",
+  projectId: "proyecto-coderhouse-79233",
+  storageBucket: "proyecto-coderhouse-79233.appspot.com",
+  messagingSenderId: "663225850460",
+  appId: "1:663225850460:web:4a01031e32ff4b81dbe499",
+  measurementId: "G-JY27R7K3EN"
+}
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
